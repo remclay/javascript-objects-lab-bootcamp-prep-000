1a74b7ea20b7b63a53db25974fee048af76eb7b7
Remmy Clay <remmy.lauren@gmail.com>
2017-10-23 06:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   recipes.butter = '1 cup';
   return recipes;
 }
+function deleteFromObjectByKey (object, key){
+  var newRecipes = Object.assign({}, recipes);
+  delete newRecipes.eggs;
+  return newRecipes;
+}
