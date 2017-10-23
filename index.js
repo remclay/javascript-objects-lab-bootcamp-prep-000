c81cab22272fe0d15549891cfe2c40229b5db5de
Remmy Clay <remmy.lauren@gmail.com>
2017-10-23 06:44:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, recipes, {chocolate: '1 cup'});
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   recipes.butter('1 cup');
   return recipes
 }
